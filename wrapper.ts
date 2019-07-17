d535849222aeb7a05750fe9bae4b3ce1a62f4498
Britton <brittonkdeets@gmail.com>
2019-07-16 22:18:02 -0400

    init this

---
+++ wrapper.ts
@@ -0,0 +1,26 @@
+
+
+// Backend API wrapper for Pivot
+module Wrapper:
+
+    URL_BASE = 'https://yqh898xngh.execute-api.us-east-1.amazonaws.com/api/';
+    URL_REGISTER = 'register/';
+    URL_LOGIN = 'login/';
+    URL_HELLO = 'hello/'
+
+
+    register(username, password){
+
+    }
+
+    login(username, password){
+
+    }
+
+    validatePassword(password1, password2){
+
+    }
+
+    validateUsername(username){
+
+    }
