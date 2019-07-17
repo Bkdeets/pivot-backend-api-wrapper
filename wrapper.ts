281791a03d11902110461457f303000d349ab86b
Britton <brittonkdeets@gmail.com>
2019-07-16 23:25:04 -0400

    Revert "basic user functions"
    This reverts commit da770e6b24f3531d7ac5df98d984028d471eae3c.

--- wrapper.ts
+++ wrapper.ts
@@ -1,46 +1,26 @@
-import axios = require('axios');
+
 
 // Backend API wrapper for Pivot
-export class Wrapper{
+module Wrapper:
 
     URL_BASE = 'https://yqh898xngh.execute-api.us-east-1.amazonaws.com/api/';
     URL_REGISTER = 'register/';
     URL_LOGIN = 'login/';
     URL_HELLO = 'hello/'
-    INSTANCE = axios.create({
-        baseURL: URL_BASE,
-        timeout: 1000,
-        headers: {}
-    });
+
 
     register(username, password){
-        // Front end will verify that password is confirmed validated
-        INSTANCE.headers['username'] = username;
-        INSTANCE.headers['password'] = password;
-        INSTANCE.post(URL_REGISTER)
-            .then(function(response){
-                console.log(response);
-            })
-            .catch(function(error){
-                console.log(error);
-            })
-            .finally(function(response){
-                return response;
-            })
+
     }
 
     login(username, password){
-        INSTANCE.headers['username'] = username;
-        INSTANCE.headers['password'] = password;
-        INSTANCE.post(URL_LOGIN)
-            .then(function(response){
-                console.log(response);
-            })
-            .catch(function(error){
-                console.log(error);
-            })
-            .finally(function(response){
-                return response;
-            })
+
     }
-}
+
+    validatePassword(password1, password2){
+
+    }
+
+    validateUsername(username){
+
+    }
