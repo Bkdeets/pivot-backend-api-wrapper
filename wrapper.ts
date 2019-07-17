f5a6428d9c3d1e5f26673dd420f51d3b28bf3ce7
Britton <brittonkdeets@gmail.com>
2019-07-16 23:58:31 -0400

    breaking changes lol

--- wrapper.ts
+++ wrapper.ts
@@ -1,23 +1,27 @@
-import axios = require('axios');
+import axios, { AxiosRequestConfig, AxiosPromise } from 'axios';
 
 // Backend API wrapper for Pivot
-export class Wrapper{
+export class Wrapper {
 
     URL_BASE = 'https://yqh898xngh.execute-api.us-east-1.amazonaws.com/api/';
     URL_REGISTER = 'register/';
     URL_LOGIN = 'login/';
-    URL_HELLO = 'hello/'
-    INSTANCE = axios.create({
-        baseURL: URL_BASE,
-        timeout: 1000,
-        headers: {}
-    });
+    URL_HELLO = 'hello/';
+    INSTANCE: any;
+
+    constructor(){
+        this.INSTANCE = axios.create({
+            baseURL: this.URL_BASE,
+            timeout: 1000,
+            headers: {}
+        });
+    }
 
     register(username, password){
         // Front end will verify that password is confirmed validated
-        INSTANCE.headers['username'] = username;
-        INSTANCE.headers['password'] = password;
-        INSTANCE.post(URL_REGISTER)
+        this.INSTANCE.headers['username'] = username;
+        this.INSTANCE.headers['password'] = password;
+        this.INSTANCE.post(this.URL_REGISTER)
             .then(function(response){
                 console.log(response);
             })
@@ -30,9 +34,9 @@ export class Wrapper{
     }
 
     login(username, password){
-        INSTANCE.headers['username'] = username;
-        INSTANCE.headers['password'] = password;
-        INSTANCE.post(URL_LOGIN)
+        this.INSTANCE.headers['username'] = username;
+        this.INSTANCE.headers['password'] = password;
+        this.INSTANCE.post(this.URL_LOGIN)
             .then(function(response){
                 console.log(response);
             })
