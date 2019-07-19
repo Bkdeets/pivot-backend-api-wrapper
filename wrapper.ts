caa74c2c818bb2cb4fbd3885996cd6569e4a69d3
Britton <brittonkdeets@gmail.com>
2019-07-18 23:47:23 -0400

    wrapper work

--- wrapper.ts
+++ wrapper.ts
@@ -3,6 +3,7 @@ import axios, { AxiosRequestConfig, AxiosPromise } from 'axios';
 // Backend API wrapper for Pivot
 export class Wrapper {
 
+    proxyurl = "https://cors-anywhere.herokuapp.com/";
     URL_BASE = 'https://yqh898xngh.execute-api.us-east-1.amazonaws.com/api/';
     URL_REGISTER = 'register/';
     URL_LOGIN = 'login/';
@@ -11,32 +12,33 @@ export class Wrapper {
 
     constructor(){
         this.INSTANCE = axios.create({
-            baseURL: this.URL_BASE,
-            timeout: 1000,
+            baseURL: this.proxyurl + this.URL_BASE,
+            timeout: 10000,
             headers: {}
         });
     }
 
     register(username, password){
         // Front end will verify that password is confirmed validated
-        this.INSTANCE.headers['username'] = username;
-        this.INSTANCE.headers['password'] = password;
-        this.INSTANCE.post(this.URL_REGISTER)
+        let data = {
+            username: username,
+            password: password
+        }
+        returnthis.INSTANCE.post(this.URL_REGISTER, data)
             .then(function(response){
                 console.log(response);
             })
             .catch(function(error){
                 console.log(error);
-            })
-            .finally(function(response){
-                return response;
-            })
+            });
     }
 
     login(username, password){
-        this.INSTANCE.headers['username'] = username;
-        this.INSTANCE.headers['password'] = password;
-        this.INSTANCE.post(this.URL_LOGIN)
+        let data = {
+            username: username,
+            password: password
+        }
+        this.INSTANCE.post(this.URL_LOGIN, data)
             .then(function(response){
                 console.log(response);
             })
